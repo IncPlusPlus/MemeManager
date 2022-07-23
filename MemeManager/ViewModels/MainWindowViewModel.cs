a115559b8f8b73a49840055896692e4648281676
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-07-23 10:33:48 -0400

    Initialize project with Avalonia

---
+++ MemeManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MemeManager.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ MemeManager/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace MemeManager.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
