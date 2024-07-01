adf6036e32836e95fe9c521bbcc12030375e58dc
Ryan Cloherty <6992149+IncPlusPlus@users.noreply.github.com>
2024-07-01 14:43:09 -0400

    Try to cut down on code duplication

--- lib/widgets/meme_list.dart
+++ lib/widgets/meme_list.dart
@@ -19,77 +19,63 @@ class _MemeListState extends State<MemeList> {
     final realmServices = Provider.of<RealmServices>(context);
     final int? selectedCategory =
         context.select((SearchModel model) => model.selectedCategory);
+    // This is an ugly, ugly hack.
+    // I wish I could combine the two stream types so I don't repeat code.
+    // The problem is one class has the results property and one has the list property.
+    // Alas, my feeble grasp of Dart prevents such a feat.
     if (selectedCategory == null) {
       return StreamBuilder<RealmResultsChanges<Meme>>(
           stream: realmServices.realm.all<Meme>().changes,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              if (snapshot.hasData &&
+          builder: (context, snapshot) => memeListBuilder(
+              context,
+              snapshot,
+              snapshot.hasData &&
                   snapshot.data != null &&
-                  snapshot.data!.results.isNotEmpty) {
-                final results = snapshot.data!.results;
-                return GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 5,
-                  ),
-                  itemCount: results.realm.isClosed ? 0 : results.length,
-                  itemBuilder: (context, index) {
-                    return results[index].isValid
-                        ? ListTile(
-                            title: Text(results[index].name),
-                            subtitle: Text(results[index].path),
-                          )
-                        : const SizedBox(height: 0);
-                  },
-                );
-              } else {
-                return const Center(
-                  child: Text('No memes found'),
-                );
-              }
-            }
-          });
+                  snapshot.data!.results.isNotEmpty,
+              snapshot.data?.results));
     } else {
       return StreamBuilder<RealmListChanges<Meme>>(
           stream: realmServices.realm
               .find<Category>(selectedCategory)!
               .memes
               .changes,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              if (snapshot.hasData &&
+          builder: (context, snapshot) => memeListBuilder(
+              context,
+              snapshot,
+              snapshot.hasData &&
                   snapshot.data != null &&
-                  snapshot.data!.list.isNotEmpty) {
-                final results = snapshot.data!.list;
-                return GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 5,
-                  ),
-                  itemCount: results.realm.isClosed ? 0 : results.length,
-                  itemBuilder: (context, index) {
-                    return results[index].isValid
-                        ? ListTile(
-                            title: Text(results[index].name),
-                            subtitle: Text(results[index].path),
-                          )
-                        : const SizedBox(height: 0);
-                  },
-                );
-              } else {
-                return const Center(
-                  child: Text('No memes found'),
-                );
-              }
-            }
-          });
+                  snapshot.data!.list.isNotEmpty,
+              snapshot.data?.list));
+    }
+  }
+
+  Widget memeListBuilder(context, snapshot, hasDataBool, resultsVar) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      if (hasDataBool) {
+        final results = resultsVar;
+        return GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 5,
+          ),
+          itemCount: results.realm.isClosed ? 0 : results.length,
+          itemBuilder: (context, index) {
+            return results[index].isValid
+                ? ListTile(
+                    title: Text(results[index].name),
+                    subtitle: Text(results[index].path),
+                  )
+                : const SizedBox(height: 0);
+          },
+        );
+      } else {
+        return const Center(
+          child: Text('No memes found'),
+        );
+      }
     }
   }
 }
